192d0c6f03fffb91c3ac8dbd9ea3550503f68f47
Maria Prates <71909004+MariaPrates@users.noreply.github.com>
2021-10-29 12:43:43 -0300

    Add files via upload
    Adicionando o formulário HTML

---
+++ script.php
@@ -0,0 +1,22 @@
+<?php
+ $categorias = [];
+ $categorias[] = "infantil";
+ $categorias[] = "adolescente";
+ $categorias[] = "adulto";
+
+
+ $nome = $_POST['nome'];
+ $idade = $_POST['idade'];
+/*if($idade >= 6 && $idade <= 12){
+for ($i=0; $i <= count($categorias); $i++){
+  //if ($categorias[$i] == 'infantil')
+  //echo "O nadador " .$nome. " compete na " .$categorias[$i];
+}
+}
+else if($idade >= 13 && $idade <= 18){
+echo "Adolescente";
+}
+else if($idade >= 18){
+echo "Adulto";
+}*/
+?>
