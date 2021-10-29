e45d6cd613208df8eb557b9746b8a31f6ff7e0f8
Maria Prates <71909004+MariaPrates@users.noreply.github.com>
2021-10-29 12:03:33 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,24 @@
 <?php
- echo "HEllo World";
+	 $categorias = [];
+	 $categorias[] = "infantil";
+	 $categorias[] = "adolescente";
+	 $categorias[] = "adulto";
+
+$nome = "Eduardo";
+$idade = 8;
+
+//var_dump ($nome,$idade);
+
+if($idade >= 6 && $idade <= 12){
+    for ($i=0; $i <= count($categorias); $i++){
+      if ($categorias[$i] == 'infantil')
+      echo "O nadador " .$nome. " compete na " .$categorias[$i];
+    }
+}
+else if($idade >= 13 && $idade <= 18){
+  echo "Adolescente";
+}
+else if($idade >= 18){
+  echo "Adulto";
+}
 ?>
