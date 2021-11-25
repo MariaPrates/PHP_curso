0251ac2cd1bda667708a3a16fd72264f25720331
MariaPrates <maria.prates0403@gmail.com>
2021-11-25 15:12:39 -0300

    Adicionando validações

--- index.php
+++ index.php
@@ -1,40 +1,49 @@
 <?php
-include "servicos/servicoMensagemSession.php";
+    include "servicos/servicoMensagemSession.php";
 ?>
 <!DOCTYPE html>
 <html>
 
 <head>
-    <meta charset="utf-8">
-    <title>Formulário de inscrição</title>
+    <title>Inscrições!</title>
+    <meta charset="UTF-8">
     <meta name="author" content="">
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="style.css" />
 </head>
-
 <body>
+    <h4 class="title">Formulário de Inscrição para Candidatos! </h4>
+        <form action="script.php" method="POST">
+            <?php 
 
-<p>FORMULÁRIO PARA INSCRIÇÃO DE COMPETIDORES</p>
+                $mensagemDeSucesso = obterMensagemSucesso();
+                    if (!empty($mensagemDeSucesso)) 
+                    {
+                        echo $mensagemDeSucesso;
+                    }
 
-<form action="script.php" method="post">
-    <?php
-        $mensagemDeSucesso = obterMensagemSucesso();
-        if(!empty($mensagemDeSucesso))
-        {
-            echo $mensagemDeSucesso;
-        }
+                $mensagemDeErro = obterMensagemErro();
+                    if (!empty($mensagemDeErro)) 
+                    {
+                        echo $mensagemDeErro;
+                    }
+                ?>
+           
+                <p>     
+                    <label for="nome">Nome completo: <em class="required">(obrigatório)</em></label>
+                    <input name="nome" id="nome" type="text" />
+                </p>
 
-        $mensagemDeErro = obterMensagemErro();
-        if(!empty($mensagemDeErro))
-        {
-            echo $mensagemDeErro;
-        }
-    ?>
-    <p>Seu nome: <input type="text" name="nome" /></p>
-    <p>Sua idade: <input type="text" name="idade" /></p>
-    <p><input type="submit" value="Enviar dados do competidor"/></p>
-</form>
+                <p>
+                    <label for="idade">Idade:</label>
+                    <input name="idade" id="idade" type="text"/>
+                </p>
 
+            <p>
+                <input type="submit" value="Enviar dados" id="enviar"/>
+            </p>
+
+        </form>
 </body>
-
 </html>
